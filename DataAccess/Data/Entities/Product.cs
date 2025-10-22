5e678dc5847319fb10032d420b5a4df03bff7f8c
Vlad Tymoshchuk <59306514+vladtymo@users.noreply.github.com>
2025-10-22 19:00:17 +0300

    Add rating

--- DataAccess/Data/Entities/Product.cs
+++ DataAccess/Data/Entities/Product.cs
@@ -7,6 +7,7 @@
         public string? ImageUrl { get; set; }
         public decimal Price { get; set; }
         public int Discount { get; set; }
+        public int Rating { get; set; }
         public int Quantity { get; set; }
         public string? Description { get; set; }
         public int CategoryId { get; set; }
